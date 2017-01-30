eb21519c0651f448dd592b1ececf0682b6b38541
taylor.zachary11@gmail.com <lDdrLdO9SxWzMfA01>
2017-01-30 17:11:43 -0600

    Adds proper MainActivity.java and fixes a drawable.

--- app/src/main/java/com/nonamehero2/blackjack/MainActivity.java
+++ app/src/main/java/com/nonamehero2/blackjack/MainActivity.java
@@ -1,7 +1,15 @@
 package com.nonamehero2.blackjack;
+/**
+ * As of now this is a rough design and we need to find some poker chips
+ * to use in addition to sprucing it up somehow.
+ */
+
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView playView = (TextView) findViewById(R.id.play_text);
+        TextView exitView = (TextView) findViewById(R.id.exit_text);
+
+        //Play button listener
+        //Need to open the betting activity when its created.
+        playView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Start Intent for betting here
+                Toast.makeText(MainActivity.this, "Play clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Exit button listener
+        //This is functional however it might not be the best way to approach exiting
+        //the application.
+        exitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
     }
 }
