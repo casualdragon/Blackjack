df8c5f6a67d6215d35bb95ffe167223b0921f1b3
Amy H <amhill@bellsouth.net>
2017-01-31 13:55:06 -0600

    created the bet activity. Created the intent for the MainActivity for the BetActivity

---
+++ app/src/main/java/com/nonamehero2/blackjack/BetActivity.java
@@ -0,0 +1,59 @@
+package com.nonamehero2.blackjack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This activity is used for interactions in the bet activity.
+ */
+
+public class BetActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bet);
+
+
+
+
+
+        //Setting up the add and subtract buttons
+        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText et = (EditText) findViewById(R.id.bet_editText);
+                TextView tv = (TextView) findViewById(R.id.total_textView);
+                String sBet = et.getText().toString();
+                Log.i("=================", "Bet " + sBet);
+                String sTotal = tv.getText().toString();
+                int bet = Integer.parseInt(sBet);
+                int total = Integer.parseInt(sTotal);
+                    bet += 100;
+                if(bet <= total) {
+                    et.setText(bet);
+                }else{
+                    Toast.makeText(getApplicationContext(), "The bet cannot exceed the total.",Toast.LENGTH_LONG);
+                }
+            }
+        });
+
+        findViewById(R.id.subtract_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText et = (EditText) findViewById(R.id.bet_editText);
+                int bet = Integer.parseInt(et.getText().toString());
+                bet -= 100;
+                if(bet > 0) {
+                    et.setText(bet);
+                }else{
+                    Toast.makeText(getApplicationContext(), "The bet cannot be less than 0.",Toast.LENGTH_LONG);
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/nonamehero2/blackjack/MainActivity.java
+++ app/src/main/java/com/nonamehero2/blackjack/MainActivity.java
@@ -5,6 +5,7 @@ package com.nonamehero2.blackjack;
  */
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Start Intent for betting here
-                Toast.makeText(MainActivity.this, "Play clicked.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), BetActivity.class);
+                startActivity(intent);
             }
         });
 
