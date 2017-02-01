c1a4b74ae68095158d1067d415d49f7591d7a558
Amy H <amhill@bellsouth.net>
2017-02-01 17:20:32 -0600

    Added comments for the constructor and the fields

--- app/src/main/java/com/nonamehero2/blackjack/Deck.java
+++ app/src/main/java/com/nonamehero2/blackjack/Deck.java
@@ -5,6 +5,7 @@ package com.nonamehero2.blackjack;
  */
 
 public class Deck {
+    //Fields
     private Card [][] deck = new Card[3][13];
     private final int row = 3;
     private final int column = 13;
@@ -12,9 +13,12 @@ public class Deck {
     //Constructor
     public Deck(){
         for(int i = 0; i < row; i++){
+            //The row is the suit of the card
             for (int j = 0; j < column; j++) {
+                //The column is the number of the card
                 deck[i][j] = new Card(i, j);
             }
         }
     }
+
 }
