86d805917e139d2e48ec65fc13d57614c17604a6
Amy H <amhill@bellsouth.net>
2017-02-01 17:13:45 -0600

    Created Deck constructor

--- app/src/main/java/com/nonamehero2/blackjack/Deck.java
+++ app/src/main/java/com/nonamehero2/blackjack/Deck.java
@@ -5,5 +5,16 @@ package com.nonamehero2.blackjack;
  */
 
 public class Deck {
+    private Card [][] deck = new Card[3][13];
+    private final int row = 3;
+    private final int column = 13;
 
+    //Constructor
+    public Deck(){
+        for(int i = 0; i < row; i++){
+            for (int j = 0; j < column; j++) {
+                deck[i][j] = new Card(i, j);
+            }
+        }
+    }
 }
