68ea1f53cea1a318e4604bba3b5e53a5a7c8a5e9
Amy H <amhill@bellsouth.net>
2017-02-03 21:56:46 -0600

    Fixed the toString method for the Card class.

--- app/src/main/java/com/nonamehero2/blackjack/GameActivity.java
+++ app/src/main/java/com/nonamehero2/blackjack/GameActivity.java
@@ -3,6 +3,7 @@ package com.nonamehero2.blackjack;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -30,6 +31,20 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        ImageView [] playerImageViews = new ImageView[5];
+        playerImageViews[0] = (ImageView) findViewById(R.id.player_imageview1);
+        playerImageViews[1] = (ImageView) findViewById(R.id.player_imageview2);
+        playerImageViews[2] = (ImageView) findViewById(R.id.player_imageview3);
+        //playerImageViews[3] = (ImageView) findViewById(R.id.player_imageview4);
+        playerImageViews[4] = (ImageView) findViewById(R.id.player_imageview5);
+
+        ImageView [] dealerImageViews = new ImageView[5];
+        dealerImageViews[0] = (ImageView) findViewById(R.id.dealer_imageview1);
+        dealerImageViews[1] = (ImageView) findViewById(R.id.dealer_imageview2);
+        dealerImageViews[2] = (ImageView) findViewById(R.id.dealer_imageview3);
+        dealerImageViews[3] = (ImageView) findViewById(R.id.dealer_imageview4);
+        dealerImageViews[4] = (ImageView) findViewById(R.id.dealer_imageview5);
+
         Intent intent = getIntent();
         int bet = intent.getIntExtra(BetActivity.BET_KEY, 0);
         /*
@@ -52,15 +67,17 @@ public class GameActivity extends AppCompatActivity {
         findViewById(R.id.hit_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i("=====================", "Hit button");
                 //Picks a random card
                 Card rand = deck.randomCard();
                 user.addCard(rand);
 
                 //Displays the card in the user's hand
 
-                //Check hand to see it is over 21
+                //Check hand to see if it is 21 - Win
                 if(user.getCardTotal() == 21){
-                    //Natural 21 - Win
+
+                //Check hand to see if it is over 21
                 }else if(user.getCardTotal() > 21){
                     //Lose - subtract bet from total
                     user.subtractBet();
